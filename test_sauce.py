5252391b247d0123856892e34f14d0ad080bc06c
Chelovek Pavek <igork1996@gmail.com>
2024-05-07 22:58:24 +0700

    modified

--- test_sauce.py
+++ test_sauce.py
@@ -3,7 +3,7 @@ import time
 
 # import prefs as prefs
 import pytest
-import allure
+# import allure
 from selenium import webdriver
 from selenium.webdriver import Keys
 from webdriver_manager.firefox import GeckoDriverManager
@@ -25,13 +25,21 @@ action = ActionChains(driver)
 wait = WebDriverWait(driver, 15, poll_frequency=1)
 
 
-@allure.feature("LOG IN")
+# @allure.feature("LOG IN")
+# def test_login():
+#     driver.get("https://www.saucedemo.com/")
+#     with allure.step("Filling Login"):
+#         driver.find_element('xpath', '//input[@id = "user-name"]').send_keys("standard_user")
+#         driver.find_element('xpath', '//input[@id = "password"]').send_keys("secret_sauce")
+#     with allure.step("Entering system"):
+#         driver.find_element("xpath", '//input[@id = "login-button"]').click()
+
+
+
 def test_login():
     driver.get("https://www.saucedemo.com/")
-    with allure.step("Filling Login"):
-        driver.find_element('xpath', '//input[@id = "user-name"]').send_keys("standard_user")
-        driver.find_element('xpath', '//input[@id = "password"]').send_keys("secret_sauce")
-    with allure.step("Entering system"):
-        driver.find_element("xpath", '//input[@id = "login-button"]').click()
+    driver.find_element('xpath', '//input[@id = "user-name"]').send_keys("standard_user")
+    driver.find_element('xpath', '//input[@id = "password"]').send_keys("secret_sauce")
+    driver.find_element("xpath", '//input[@id = "login-button"]').click()
 
 
